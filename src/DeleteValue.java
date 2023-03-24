51f11bfed502140bea975fd4ee281e42c97997d2
tuuyen09 <tu190901@gmail.com>
2023-03-24 17:10:21 +0700

    DeleteValue

--- src/DeleteValue.java
+++ src/DeleteValue.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class DeleteValue {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DeleteValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int index = 0;
+        System.out.println("Enter x:");
+        int x = scanner.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]) {
+                index = i;
+                arr[arr.length - 1] = 0;
+            }
+        }
+        for (int j = index; j < arr.length - 1; j++) {
+            arr[j] = arr[j + 1];
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
+
