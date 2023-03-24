f9508d907b10d15742e400549731f3587e237a83
tuuyen09 <tu190901@gmail.com>
2023-03-24 10:07:52 +0700

    Min_in_array

--- src/Min_in_array.java
+++ src/Min_in_array.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Min_in_array {
+public class Min_in_array {
+    public static void main(String[] args) {
+     int[] array = {1,2,3,4,5};
+     int index = minValue(array);
+        System.out.println("Min = " + array[index]);
+    }
+    public static int minValue (int[] arr){
+        int min = arr[0];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (min> arr[i]){
+                min = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
 }
