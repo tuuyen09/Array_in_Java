e075fd9a228f647e8c442dc0efa3a6bd18cc45b6
tuuyen09 <tu190901@gmail.com>
2023-03-24 17:09:16 +0700

    AddValue

--- src/AddValue.java
+++ src/AddValue.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AddValue {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println("Enter x:");
+        int x = scanner.nextInt();
+        System.out.println("Enter index:");
+        int index = scanner.nextInt();
+
+        for (int i = arr.length - 1; i >= index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = x;
+        System.out.println(Arrays.toString(arr));
+    }
 }
